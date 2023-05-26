a967a4ea2783ff3327930277a8e01599cca95b1d
subiktha shalom <subiktha.shalom0302@gmail.com>
2023-05-26 18:07:40 +0530

    second changes made by shalom

--- PurpleAdmin-Free-Admin-Template-master/pages/forms/upload.php
+++ PurpleAdmin-Free-Admin-Template-master/pages/forms/upload.php
@@ -1,51 +1,72 @@
-
 <?php
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'uploaded';
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "registration_db";
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
+// SQL query for creating a table
 
 
 
-$targetDir = "uploads/";  // Directory where you want to store the uploaded files
-$targetFile = $targetDir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;  // Flag to check if the file upload is valid
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $targetDir = "uploads/"; // Directory to store uploaded images
+    $targetFile = $targetDir . basename($_FILES["image"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-// Check if a file has been selected
-if (!empty($_FILES["fileToUpload"]["tmp_name"])) {
+    // Check if the file is an actual image
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    if ($check === false) {
+        echo "Error: File is not an image.";
+        $uploadOk = 0;
+    }
+
     // Check if the file already exists
     if (file_exists($targetFile)) {
-        echo "File already exists.";
+        echo "Error: File already exists.";
         $uploadOk = 0;
     }
 
-    // Check file size (optional)
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "File is too large. Maximum file size allowed is 500KB.";
+    // Check file size
+    if ($_FILES["image"]["size"] > 500000) {
+        echo "Error: File is too large.";
         $uploadOk = 0;
     }
 
-    // Check file format (optional)
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");  // Example list of allowed file extensions
-    $fileExtension = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-    if (!in_array($fileExtension, $allowedExtensions)) {
-        echo "Invalid file format. Only JPG, JPEG, PNG, and GIF files are allowed.";
+    // Allow only certain file formats
+    if ($imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "png" && $imageFileType != "gif") {
+        echo "Error: Only JPG, JPEG, PNG, and GIF files are allowed.";
         $uploadOk = 0;
     }
 
-    // If the upload is valid, move the file to the target directory
-    if ($uploadOk) {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-            echo "File uploaded successfully.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
+    // If no errors, move the file to the target directory and insert filename into the database
+    if ($uploadOk == 1) {
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
+            $filename = basename($_FILES["image"]["name"]);
+            $insertSql = "INSERT INTO images (filename) VALUES ('$filename')";
+           
     }
+}
+
+// Display uploaded images
+$sql = "SELECT filename FROM images";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $imagePath = "uploads/" . $row["filename"];
+        echo '<img src="' . $imagePath . '" alt="Uploaded Image">';
+    }
+} else {
+    echo "No images found.";
+}
+
+$conn->close();
 }
 ?>
