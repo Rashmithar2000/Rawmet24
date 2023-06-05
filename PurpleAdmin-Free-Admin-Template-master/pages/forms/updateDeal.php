aebd40bd3be1b2e4f22cddd4311cfeaf9e792218
rashmitha <rashpriya2211@gmail.com>
2023-06-05 15:28:13 +0530

    update changes

--- PurpleAdmin-Free-Admin-Template-master/pages/forms/updateDeal.php
+++ PurpleAdmin-Free-Admin-Template-master/pages/forms/updateDeal.php
@@ -1,60 +1,36 @@
 <?php
 include_once "connect.php";
-// $id= $_GET['updateid'];
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $id = isset($_POST['id']) ? $_POST['id'] : '';
-    $category = isset($_POST['category']) ? $_POST['category'] : '';
-    $infoId = isset($_POST['infoId']) ? $_POST['infoId'] : '';
-    $ownership = isset($_POST['ownership']) ? $_POST['ownership'] : '';
-    $tenderLocation = isset($_POST['tenderLocation']) ? $_POST['tenderLocation'] : '';
-    $sector = isset($_POST['sector']) ? $_POST['sector'] : '';
-    $tenderNumber = isset($_POST['tenderNumber']) ? $_POST['tenderNumber'] : '';
-    $tenderDesc = isset($_POST['tenderDesc']) ? $_POST['tenderDesc'] : '';
-    $material = isset($_POST['material']) ? $_POST['material'] : '';
-    $quantity = isset($_POST['quantity']) ? $_POST['quantity'] : ''; 
-    $unit = isset($_POST['unit']) ? $_POST['unit'] : '';
-    $value = (isset($_POST['quantity']) ? $_POST['quantity'] : '') . ' ' . (isset($_POST['unit']) ? $_POST['unit'] : '');
-    $publishingDatetime = isset($_POST['publishingDatetime']) ? $_POST['publishingDatetime'] : '';
-    $startDatetime = isset($_POST['startDatetime']) ? $_POST['startDatetime'] : '';
-    $endDatetime = isset($_POST['endDatetime']) ? $_POST['endDatetime'] : '';
-    $emdType = isset($_POST['emdType']) ? $_POST['emdType'] : '';
-    $emdAmt = isset($_POST['emdAmt']) ? $_POST['emdAmt'] : '';
-    $tenderValue = isset($_POST['tenderValue']) ? $_POST['tenderValue'] : '';
-    $companyName = isset($_POST['companyName']) ? $_POST['companyName'] : '';
-    $location = isset($_POST['location']) ? $_POST['location'] : '';
-    $street = isset($_POST['street']) ? $_POST['street'] : '';
-    $city = isset($_POST['city']) ? $_POST['city'] : '';
-    $telephone = isset($_POST['telephone']) ? $_POST['telephone'] : '';
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $contactPerson = isset($_POST['contactPerson']) ? $_POST['contactPerson'] : '';
-
-   
-
-    $category = mysqli_real_escape_string($conn, $category);
-    $infoId = mysqli_real_escape_string($conn, $infoId);
-    $ownership = mysqli_real_escape_string($conn, $ownership);
-    $tenderLocation = mysqli_real_escape_string($conn, $tenderLocation);
-    $sector = mysqli_real_escape_string($conn, $sector);
-    $tenderNumber = mysqli_real_escape_string($conn, $tenderNumber);
-    $tenderDesc = mysqli_real_escape_string($conn, $tenderDesc);
-    $material = mysqli_real_escape_string($conn, $material);
-    $quantity = mysqli_real_escape_string($conn, $quantity);
-    $value = mysqli_real_escape_string($conn, $value);
-    $publishingDatetime = mysqli_real_escape_string($conn, $publishingDatetime);
-    $startDatetime = mysqli_real_escape_string($conn, $startDatetime);
-    $endDatetime = mysqli_real_escape_string($conn, $endDatetime);
-    $emdType = mysqli_real_escape_string($conn, $emdType);
-    $emdAmt = mysqli_real_escape_string($conn, $emdAmt);
-    $tenderValue = mysqli_real_escape_string($conn, $tenderValue);
-    $companyName = mysqli_real_escape_string($conn, $companyName);
-    $location = mysqli_real_escape_string($conn, $location);
-    $street = mysqli_real_escape_string($conn, $street);
-    $city = mysqli_real_escape_string($conn, $city);
-    $telephone = mysqli_real_escape_string($conn, $telephone);
-    $email = mysqli_real_escape_string($conn, $email);
-    $contactPerson = mysqli_real_escape_string($conn, $contactPerson);
-
     
+    $id = isset($_POST['id']) ? mysqli_real_escape_string($conn, $_POST['id']) : '';
+    $category = isset($_POST['category']) ? mysqli_real_escape_string($conn, $_POST['category']) : '';
+    $infoId = isset($_POST['infoId']) ? mysqli_real_escape_string($conn, $_POST['infoId']) : '';
+    $ir = isset($_POST['ir']) ? mysqli_real_escape_string($conn, $_POST['ir']) : '';
+    $fe = isset($_POST['fe']) ? mysqli_real_escape_string($conn, $_POST['fe']) : '';
+    $quantity = isset($_POST['quantity']) ? mysqli_real_escape_string($conn, $_POST['quantity']) : '';
+    $unit = isset($_POST['unit']) ? mysqli_real_escape_string($conn, $_POST['unit']) : '';
+    $dealValue = isset($_POST['dealValue']) ? mysqli_real_escape_string($conn, $_POST['dealValue']) : '';
+    $dealDatetime = isset($_POST['dealDatetime']) ? mysqli_real_escape_string($conn, $_POST['dealDatetime']) : '';
+    $docCreatedby = isset($_POST['docCreatedby']) ? mysqli_real_escape_string($conn, $_POST['docCreatedby']) : '';
+    $location = isset($_POST['location']) ? mysqli_real_escape_string($conn, $_POST['location']) : '';
+    $industrialArea = isset($_POST['industrialArea']) ? mysqli_real_escape_string($conn, $_POST['industrialArea']) : '';
+    $companyName = isset($_POST['companyName']) ? mysqli_real_escape_string($conn, $_POST['companyName']) : '';
+    $contactPerson = isset($_POST['contactPerson']) ? mysqli_real_escape_string($conn, $_POST['contactPerson']) : '';
+    $email = isset($_POST['email']) ? mysqli_real_escape_string($conn, $_POST['email']) : '';
+    $address = isset($_POST['address']) ? mysqli_real_escape_string($conn, $_POST['address']) : '';
+    $city = isset($_POST['city']) ? mysqli_real_escape_string($conn, $_POST['city']) : '';
+    $state = isset($_POST['state']) ? mysqli_real_escape_string($conn, $_POST['state']) : '';
+    $contactNumber = isset($_POST['contactNumber']) ? mysqli_real_escape_string($conn, $_POST['contactNumber']) : '';
+    $whatsappNumber = isset($_POST['whatsappNumber']) ? mysqli_real_escape_string($conn, $_POST['whatsappNumber']) : '';
+    $gstin = isset($_POST['gstin']) ? mysqli_real_escape_string($conn, $_POST['gstin']) : '';
+    $orderType = isset($_POST['orderType']) ? mysqli_real_escape_string($conn, $_POST['orderType']) : '';
+    $material = isset($_POST['material']) ? mysqli_real_escape_string($conn, $_POST['material']) : '';
+    $specification = isset($_POST['specification']) ? mysqli_real_escape_string($conn, $_POST['specification']) : '';
+    $dor = isset($_POST['dor']) ? mysqli_real_escape_string($conn, $_POST['dor']) : '';
+    $expQuotation = isset($_POST['expQuotation']) ? mysqli_real_escape_string($conn, $_POST['expQuotation']) : '';
+    
+
     $value = isset($_POST['value']) ? $_POST['value'] : '';
 
     if (!empty($value)) {
@@ -120,39 +96,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
    $value = $quantity . ' ' . $unit;
 
     // Build the query
-    $sql = "UPDATE tenders SET 
+    $sql = "UPDATE deals SET
         category = '$category',
         infoId = '$infoId',
-        ownership = '$ownership',
-        tenderLocation = '$tenderLocation',
-        sector = '$sector',
-        tenderNumber = '$tenderNumber',
-        tenderDesc = '$tenderDesc',
-        material = '$material',
+        ir = '$ir',
+        fe = '$fe',
         quantity = '$value',
-        publishingDatetime = '$publishingDatetime',
-        startDatetime = '$startDatetime',
-        endDatetime = '$endDatetime',
-        emdType = '$emdType',
-        emdAmt = '$emdAmt',
-        tenderValue = '$tenderValue',
-        companyName = '$companyName',
+        dealValue = '$dealValue',
+        dealDatetime = '$dealDatetime',
+        docCreatedby = '$docCreatedby',
         location = '$location',
-        street = '$street',
-        city = '$city',
-        telephone = '$telephone',
-        email = '$email',
+        industrialArea = '$industrialArea',
+        companyName = '$companyName',
         contactPerson = '$contactPerson',
-        img = '$fname2',
-        file = '$dname2'
+        email = '$email',
+        address = '$address',
+        city = '$city',
+        state = '$state',
+        contactNumber = '$contactNumber',
+        whatsappNumber = '$whatsappNumber',
+        gstin = '$gstin',
+        orderType = '$orderType',
+        material = '$material',
+        specification = '$specification',
+        dor = '$dor',
+        expQuotation = '$expQuotation'
         WHERE id = '$id'";
 
     if (mysqli_query($conn, $sql)) {
 
         ?>
         <script>
-            alert("Tender Updated Successfully!");
-            window.location.replace("./tenders.php");
+            alert("Deals Updated Successfully!");
+            window.location.replace("./deals.php");
         </script>
         <?php
     } else {
--- PurpleAdmin-Free-Admin-Template-master/pages/forms/updateTend.php
+++ PurpleAdmin-Free-Admin-Template-master/pages/forms/updateTend.php
@@ -2,59 +2,32 @@
 include_once "connect.php";
 // $id= $_GET['updateid'];
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $id = isset($_POST['id']) ? $_POST['id'] : '';
-    $category = isset($_POST['category']) ? $_POST['category'] : '';
-    $infoId = isset($_POST['infoId']) ? $_POST['infoId'] : '';
-    $ownership = isset($_POST['ownership']) ? $_POST['ownership'] : '';
-    $tenderLocation = isset($_POST['tenderLocation']) ? $_POST['tenderLocation'] : '';
-    $sector = isset($_POST['sector']) ? $_POST['sector'] : '';
-    $tenderNumber = isset($_POST['tenderNumber']) ? $_POST['tenderNumber'] : '';
-    $tenderDesc = isset($_POST['tenderDesc']) ? $_POST['tenderDesc'] : '';
-    $material = isset($_POST['material']) ? $_POST['material'] : '';
-    $quantity = isset($_POST['quantity']) ? $_POST['quantity'] : ''; 
-    $unit = isset($_POST['unit']) ? $_POST['unit'] : '';
-    $value = (isset($_POST['quantity']) ? $_POST['quantity'] : '') . ' ' . (isset($_POST['unit']) ? $_POST['unit'] : '');
-    $publishingDatetime = isset($_POST['publishingDatetime']) ? $_POST['publishingDatetime'] : '';
-    $startDatetime = isset($_POST['startDatetime']) ? $_POST['startDatetime'] : '';
-    $endDatetime = isset($_POST['endDatetime']) ? $_POST['endDatetime'] : '';
-    $emdType = isset($_POST['emdType']) ? $_POST['emdType'] : '';
-    $emdAmt = isset($_POST['emdAmt']) ? $_POST['emdAmt'] : '';
-    $tenderValue = isset($_POST['tenderValue']) ? $_POST['tenderValue'] : '';
-    $companyName = isset($_POST['companyName']) ? $_POST['companyName'] : '';
-    $location = isset($_POST['location']) ? $_POST['location'] : '';
-    $street = isset($_POST['street']) ? $_POST['street'] : '';
-    $city = isset($_POST['city']) ? $_POST['city'] : '';
-    $telephone = isset($_POST['telephone']) ? $_POST['telephone'] : '';
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $contactPerson = isset($_POST['contactPerson']) ? $_POST['contactPerson'] : '';
-
+    $id = isset($_POST['id']) ? mysqli_real_escape_string($conn, $_POST['id']) : '';
+    $category = isset($_POST['category']) ? mysqli_real_escape_string($conn, $_POST['category']) : '';
+    $infoId = isset($_POST['infoId']) ? mysqli_real_escape_string($conn, $_POST['infoId']) : '';
+    $ownership = isset($_POST['ownership']) ? mysqli_real_escape_string($conn, $_POST['ownership']) : '';
+    $tenderLocation = isset($_POST['tenderLocation']) ? mysqli_real_escape_string($conn, $_POST['tenderLocation']) : '';
+    $sector = isset($_POST['sector']) ? mysqli_real_escape_string($conn, $_POST['sector']) : '';
+    $tenderNumber = isset($_POST['tenderNumber']) ? mysqli_real_escape_string($conn, $_POST['tenderNumber']) : '';
+    $tenderDesc = isset($_POST['tenderDesc']) ? mysqli_real_escape_string($conn, $_POST['tenderDesc']) : '';
+    $material = isset($_POST['material']) ? mysqli_real_escape_string($conn, $_POST['material']) : '';
+    $quantity = isset($_POST['quantity']) ? mysqli_real_escape_string($conn, $_POST['quantity']) : '';
+    $unit = isset($_POST['unit']) ? mysqli_real_escape_string($conn, $_POST['unit']) : '';
+    $value = (isset($_POST['quantity']) ? mysqli_real_escape_string($conn, $_POST['quantity']) : '') . ' ' . (isset($_POST['unit']) ? mysqli_real_escape_string($conn, $_POST['unit']) : '');
+    $publishingDatetime = isset($_POST['publishingDatetime']) ? mysqli_real_escape_string($conn, $_POST['publishingDatetime']) : '';
+    $startDatetime = isset($_POST['startDatetime']) ? mysqli_real_escape_string($conn, $_POST['startDatetime']) : '';
+    $endDatetime = isset($_POST['endDatetime']) ? mysqli_real_escape_string($conn, $_POST['endDatetime']) : '';
+    $emdType = isset($_POST['emdType']) ? mysqli_real_escape_string($conn, $_POST['emdType']) : '';
+    $emdAmt = isset($_POST['emdAmt']) ? mysqli_real_escape_string($conn, $_POST['emdAmt']) : '';
+    $tenderValue = isset($_POST['tenderValue']) ? mysqli_real_escape_string($conn, $_POST['tenderValue']) : '';
+    $companyName = isset($_POST['companyName']) ? mysqli_real_escape_string($conn, $_POST['companyName']) : '';
+    $location = isset($_POST['location']) ? mysqli_real_escape_string($conn, $_POST['location']) : '';
+    $street = isset($_POST['street']) ? mysqli_real_escape_string($conn, $_POST['street']) : '';
+    $city = isset($_POST['city']) ? mysqli_real_escape_string($conn, $_POST['city']) : '';
+    $telephone = isset($_POST['telephone']) ? mysqli_real_escape_string($conn, $_POST['telephone']) : '';
+    $email = isset($_POST['email']) ? mysqli_real_escape_string($conn, $_POST['email']) : '';
+    $contactPerson = isset($_POST['contactPerson']) ? mysqli_real_escape_string($conn, $_POST['contactPerson']) : '';
    
-
-    $category = mysqli_real_escape_string($conn, $category);
-    $infoId = mysqli_real_escape_string($conn, $infoId);
-    $ownership = mysqli_real_escape_string($conn, $ownership);
-    $tenderLocation = mysqli_real_escape_string($conn, $tenderLocation);
-    $sector = mysqli_real_escape_string($conn, $sector);
-    $tenderNumber = mysqli_real_escape_string($conn, $tenderNumber);
-    $tenderDesc = mysqli_real_escape_string($conn, $tenderDesc);
-    $material = mysqli_real_escape_string($conn, $material);
-    $quantity = mysqli_real_escape_string($conn, $quantity);
-    $value = mysqli_real_escape_string($conn, $value);
-    $publishingDatetime = mysqli_real_escape_string($conn, $publishingDatetime);
-    $startDatetime = mysqli_real_escape_string($conn, $startDatetime);
-    $endDatetime = mysqli_real_escape_string($conn, $endDatetime);
-    $emdType = mysqli_real_escape_string($conn, $emdType);
-    $emdAmt = mysqli_real_escape_string($conn, $emdAmt);
-    $tenderValue = mysqli_real_escape_string($conn, $tenderValue);
-    $companyName = mysqli_real_escape_string($conn, $companyName);
-    $location = mysqli_real_escape_string($conn, $location);
-    $street = mysqli_real_escape_string($conn, $street);
-    $city = mysqli_real_escape_string($conn, $city);
-    $telephone = mysqli_real_escape_string($conn, $telephone);
-    $email = mysqli_real_escape_string($conn, $email);
-    $contactPerson = mysqli_real_escape_string($conn, $contactPerson);
-
-    
     $value = isset($_POST['value']) ? $_POST['value'] : '';
 
     if (!empty($value)) {
@@ -62,6 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $quantity = isset($valueParts[0]) ? $valueParts[0] : '';
         $unit = isset($valueParts[1]) ? $valueParts[1] : '';
     }
+
 
     // ... File upload code ...
 
