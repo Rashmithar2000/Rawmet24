0ba8e7804a66ff1f5cf751f6a70c067e40c26d20
rashmitha <rashpriya2211@gmail.com>
2023-06-10 13:01:33 +0530

    notification changes

--- tender/check.php
+++ tender/check.php
@@ -1,9 +1,8 @@
 <?php
-
+session_start();
 include_once "connect.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
     $id = isset($_POST['id']) ? mysqli_real_escape_string($con, $_POST['id']) : '';
 
     $sql = "SELECT * FROM exclusive_deals WHERE id = '$id'";
@@ -15,21 +14,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $status = $row['status'];
 
             if ($status == 1) {
-                ?> <script>
-                alert("This exclusive deal is already blocked.");
-                window.history.go(-1);
-            </script>
-            <?php
-            
-            } elseif ($status == 0) {
-                $updateSql = "UPDATE exclusive_deals SET status = 1 WHERE id = '$id'";
-                if (mysqli_query($con, $updateSql)) {
-                    ?> <script>
-                    alert("Exclusive deal blocked successfully!");
+                ?>
+                <script>
+                    alert("This exclusive deal is already blocked.");
                     window.history.go(-1);
                 </script>
                 <?php
-                   
+            } elseif ($status == 0) {
+                $updateSql = "UPDATE exclusive_deals SET status = 1 WHERE id = '$id'";
+                if (mysqli_query($con, $updateSql)) {
+                    $name = $_SESSION['buyer_name']; 
+                    $timestamp = date('Y-m-d H:i:s');
+                  
+                    $notificationSql = "INSERT INTO notification (name, exclusiveDeal_id, status, timestamp) VALUES ('$name','$id', 1,'$timestamp')";
+                    if (mysqli_query($con, $notificationSql)) {
+                        ?>
+                        <script>
+                            alert("Exclusive deal blocked successfully by <?php echo $name; ?>!");
+                            window.history.go(-1);
+                        </script>
+                        <?php
+                    } else {
+                        echo "Error inserting notification: " . mysqli_error($con);
+                    }
                 } else {
                     echo "Error updating status: " . mysqli_error($con);
                 }
--- tender/deal_page.php
+++ tender/deal_page.php
@@ -296,9 +296,34 @@ h4,
          </form>
        
        
-         <?php }else{ ?>
-          <div class="user" style="padding-top:25px;padding-right:30px"> <p style="color:#3b8beb;">
-          Hi! <?php echo $_SESSION['buyer_name'];?>&nbsp&nbsp
+         <?php }else{
+
+// Retrieve the buyer's name
+$name = $_SESSION['buyer_name'];
+
+// Query to get the count of exclusive deal IDs for the particular buyer
+$countSql = "SELECT COUNT(DISTINCT exclusiveDeal_id) AS notificationCount FROM notification WHERE name = '$name'";
+$countResult = mysqli_query($con, $countSql);
+
+if ($countResult) {
+    $r= mysqli_fetch_assoc($countResult);
+    $notificationCount = $r['notificationCount'];
+} else {
+    $notificationCount = 0;
+}
+
+?>
+
+          
+          <div class="user" style="padding-top:25px;padding-right:30px"> <p style="color:#3b8beb;"> 
+          <span style="position: relative;">
+    <i class="fa-solid fa-bell" style="color:black;"></i>
+    <?php if ($notificationCount > 0) { ?>
+        <sup style="top: -10px; background-color: red; color: white; border-radius: 30%; padding: 2px 4px;"><?php echo $notificationCount; ?></sup>
+    <?php } ?>
+</span>
+          
+         &nbsp&nbsp Hi! <?php echo $_SESSION['buyer_name'];?>&nbsp&nbsp
               <button class="btn btn-primary" ><a href="signout.php" style="color:white;"> Signout </a></button>
 
 
