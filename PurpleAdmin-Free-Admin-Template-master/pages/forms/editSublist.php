873c07eb46a56d2d1fe7f5fbf5a88e8b58e8f8c6
rashmitha <rashpriya2211@gmail.com>
2023-06-12 13:58:29 +0530

    changes

--- PurpleAdmin-Free-Admin-Template-master/pages/forms/editSublist.php
+++ PurpleAdmin-Free-Admin-Template-master/pages/forms/editSublist.php
@@ -195,6 +195,20 @@ include "connect.php";
               <div class="collapse" id="ui-basic5">
                 <ul class="nav flex-column sub-menu">
                   <li class="nav-item"> <a class="nav-link" href="editSublist.php">Edit Subscriber</a></li>
+                  
+                </ul>
+                
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic6" aria-expanded="false" aria-controls="ui-basic">
+              <span class="menu-title">Metal Price List</span>
+                <i class="menu-arrow"></i>
+                <i class="mdi mdi-archive menu-icon"></i>
+              </a>
+              <div class="collapse" id="ui-basic6">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="editMetlist.php">Edit Metal Price</a></li>
                 </ul>
               </div>
             </li>
